98e4d65286e40bf35086677e832ec8d74d82f642
tglltgl <tanyap392@gmail.com>
2026-03-30 00:49:50 +0500

    Add files via upload
    не влезли нормально файлы

---
+++ src/main/java/com/example/praktica3/Player.kt
@@ -0,0 +1,35 @@
+package com.example.praktica3
+
+import androidx.annotation.DrawableRes
+
+data class Player(
+    val id: Int,
+    val name: String,
+    val number: Int,
+    val age: Int,
+    val position: String,
+    val team: String,
+    val stats: List<Pair<String, Long>>,
+    @DrawableRes val photoRes: Int
+)
+
+val mockPlayers = listOf(
+    Player(
+        id = 1, name = "Alexey Ionov", number = 11, age = 35,
+        position = "Полузащитник", team = "ФК Урал Екатеринбург",
+        stats = listOf("4 игр" to 0xFF757575, "1 гол" to 0xFF4DB6AC),
+        photoRes = R.drawable.ionov
+    ),
+    Player(
+        id = 2, name = "Erik Bicfalvi", number = 10, age = 36,
+        position = "Полузащитник", team = "ФК Урал Екатеринбург",
+        stats = listOf("2 игры" to 0xFF757575),
+        photoRes = R.drawable.bicfalvi
+    ),
+    Player(
+        id = 3, name = "Yuriy Zheleznov", number = 14, age = 21,
+        position = "Полузащитник", team = "ФК Урал Екатеринбург",
+        stats = listOf("5 игр" to 0xFF757575),
+        photoRes = R.drawable.zheleznov
+    )
+)
